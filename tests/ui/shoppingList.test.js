d21b0bea3a2cb6388ecda0d18777b0d16ed10dd9
Ivaylo Hasov <ivailohasov@gmail.com>
2023-12-16 14:32:12 +0200

    Add ui test to the ci/cd

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
-    await page.goto('http://localhost:8080/shopping-list');
+    await page.goto('https://ivailo06-exam.onrender.com/shopping-list');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
